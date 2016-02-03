6e495a6c24d8ca6d6a6dffb944a178eeddb2074f
ccrock <cam.crockett.12@gmail.com>
2016-02-02 19:11:46 -0700

    Updating asset compile

--- AOne/grails-app/conf/Config.groovy
+++ AOne/grails-app/conf/Config.groovy
@@ -121,7 +121,6 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
-
 grails.assets.minifyJs=false
 grails.assets.minifyCss=false
-grails.assets.enableSourceMaps=false
+grails.assets.includes = ["**/*.js"]
